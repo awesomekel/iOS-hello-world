a366ba7a9fe907bdd6ebb950a39418f2f8b03f96
Kel Zhang <awesomekel@Qiwens-MacBook-Pro-2.local>
2017-06-03 20:05:23 +0800

    added a button with Hello, World! on it

--- HelloXcode/ViewController.swift
+++ HelloXcode/ViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonPressed(sender: UIButton) {
+        print("Hello, World")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    // MARK: - memory -
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
